9a2ed67cfce1b33872ada6b4a7ef2c22f541552e
Bavanaa Loganathan <bavanaalogan@gmail.com>
2026-04-27 07:59:55 +0530

    Replace FreqDist with Counter for frequency count

--- experiment 02/02-a.py
+++ experiment 02/02-a.py
@@ -1,8 +1,5 @@
-import nltk
-from nltk.probability import FreqDist
 import re
-
-nltk.download('punkt_tab')
+from collections import Counter
 
 text = """
 Natural Language Processing is a branch of Artificial Intelligence.
@@ -11,7 +8,7 @@ Language processing is widely used in speech and text analysis applications.
 """
 
 word_tokens = re.findall(r'\b[a-zA-Z]+\b', text.lower())
-freq_dist = FreqDist(word_tokens)
+freq_dist = Counter(word_tokens)
 
 print("TOKENS:", word_tokens)
 print("TOP 10 MOST FREQUENT WORDS:",freq_dist.most_common(10))
